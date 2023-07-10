e706b4df51c32d3f5e7d4173b1a4e0c685a39124
Anand kumar <anandkumar.triden@gmail.com>
2023-07-10 19:14:57 +0530

    initial quotation

---
+++ src/components/app-bar/app-bar.tsx
@@ -0,0 +1,39 @@
+import { FC, useEffect, useState } from 'react';
+
+// Mui components.
+import { AppBar as MuiAppBar, Container, IconButton, Typography, Box } from '@mui/material';
+
+// Config.
+import { AppConfig } from '@/config';
+import { grey } from '@mui/material/colors';
+
+const AppBar: FC = () => {
+  const [zIndex, setZIndex] = useState<number>(3);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const scrollCheck = window.scrollY;
+      if (scrollCheck > 60) {
+        setZIndex(1);
+      } else {
+        setZIndex(3);
+      }
+    });
+  }, []);
+
+  return (
+    <MuiAppBar elevation={0} color="transparent" sx={{ py: 5, zIndex }}>
+      <Container sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+        <Box>
+          <Typography variant="h2" component="h1" color="primary.contrastText" sx={{ mb: 1 }}>
+            {AppConfig.appName}
+          </Typography>
+          <Typography sx={{ color: grey[100] }}>{AppConfig.appDescription}</Typography>
+        </Box>
+        <Box sx={{ ml: 'auto' }}></Box>
+      </Container>
+    </MuiAppBar>
+  );
+};
+
+export default AppBar;
---
+++ src/config/common/app-config.ts
@@ -0,0 +1,7 @@
+import type { IAppConfig } from './interface';
+
+export const AppConfig: IAppConfig = {
+  appName: 'Quotation Generator',
+  appDescription: 'Just a simple PDF Quotation generator.',
+  //appRepo: 'https://github.com/hiriski/react-simple-invoice-generator/tree/master',
+};
